96c7ebbebaf07938926e19c03c9eb29d6cce0d34
Scott Hansen <scottiehansen@gmail.com>
2021-05-29 23:57:09 -0700

    Working model for step 2.5

--- server/db/index.js
+++ server/db/index.js
@@ -1,1 +1,21 @@
 // TODO: Establish connection to mysql database
+const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'pokedex'
+})
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (err) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('db connected')
+  }
+})
+
+module.exports = connection;
